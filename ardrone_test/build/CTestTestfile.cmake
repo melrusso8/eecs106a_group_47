1c113bfd13c2265eadc4407f9de06795b123601c
Han Nguyen <hanhn@berkeley.edu>
2021-11-29 21:59:54 -0800

    Added ardrone test package

---
+++ ardrone_test/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eecs106a/project/src
+# Build directory: /home/eecs106a/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(drone_application)
---
+++ ardrone_test/build/drone_application/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eecs106a/project/src/drone_application
+# Build directory: /home/eecs106a/project/build/drone_application
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
