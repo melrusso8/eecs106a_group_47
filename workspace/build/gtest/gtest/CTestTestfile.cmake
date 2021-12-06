e36ddee7efa1a4770743cf74689a15fe20727450
Class Account <ee106a-aal@c105-2.local>
2021-12-05 16:16:21 -0800

    first implementation of mapping node

--- workspace/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/eecs106a/eecs106a_group_47/workspace/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- workspace/src/drone_application/src/mel_test.py
+++ workspace/src/drone_application/src/mel_test.py
@@ -5,11 +5,17 @@ from takeoff_move_land import move_forward, takeoff, turn_in_place
 from std_msgs.msg import String
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
+from ardrone_autonomy.msg import Navdata
+from nav_msgs.msg import Odometry
 
-def map_the_grid(grid_width, grid_height, res=3):
+def map_the_grid(grid_width, grid_height, x_step=3):
     # assume drone is placed at the lower left corner of rectangular grid, facing long edge
+    rospy.Subscriber('ardrone/navdata', Navdata, navdata_listener_callback)
+    rospy.Subscriber('ardrone/odometry', Odometry, odometry_listener_callback)
+
     x = 0
-    current_pos = (0,0)
+    drone_at_top = False
+    turn_angle = 90
 
     # take off
     takeoff()
@@ -17,36 +23,33 @@ def map_the_grid(grid_width, grid_height, res=3):
     # traverse the grid
     while x < grid_width:
         move_forward(grid_height)
-        turn_in_place(90)
-        move_forward(res)
-        x += res
-        turn_in_place(90)
-        current_pos = (x, grid_height)
-        if x < grid_width:
-            move_forward(grid_height)
-            turn_in_place(-90)
-            move_forward(res)
-            x += res
-            turn_in_place(-90)
-            current_pos = (x, 0)
+        turn_in_place(turn_angle)
+        move_forward(x_step)
+        x += x_step
+        turn_in_place(turn_angle)
+        drone_at_top = not drone_at_top
+        turn_angle = -turn_angle
 
     # return drone to starting position
-    if current_pos[1] == 0:
+    if not drone_at_top:
         # drone starts by facing long edge, at bottom right corner
         turn_in_place(-90)
         move_forward(x)
         turn_in_place(90)
-    if current_pos[1] == grid_height:
+    else:
         # drone starts by facing long edge, at top right corner
         move_forward(grid_height)
         turn_in_place(90)
         move_forward(x)
         turn_in_place(90)
 
-    
+def navdata_listener_callback(navdata):
+    tag_locations = zip(tags_xc, tags_yc)
+    # expressed in numbers between [0,1000]
+    # need to convert to pixel units using camera resolution from camera_info topic
 
+def odometry_listener_callback(odometry):
+    drone_pose = odometry.pose.pose
 
-
-
-
-
+if __name__ == '__main__':
+        rospy.init_node('mapping', anonymous=True)
