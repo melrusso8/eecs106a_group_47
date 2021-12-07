aa27a8a4dfd49ffc50f4c09fbd6a92040741df85
eecs106a <nicole.m.stokowski@gmail.com>
2021-12-07 10:32:46 -0800

    drone stuff

--- workspace/src/drone_application/src/takeoff_move_land.py
+++ workspace/src/drone_application/src/takeoff_move_land.py
@@ -5,32 +5,44 @@ from std_msgs.msg import String
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
 
+
+def zero_out_params():
+		pub = rospy.Publisher("cmd_vel", Twist, queue_size=10, latch=True)
+		rate = rospy.Rate(10)
+		command_zero = Twist()
+		command_zero.angular.x = 0
+		command_zero.angular.y = 0
+		command_zero.angular.z = 0
+		command_zero.linear.x = 0
+		command_zero.linear.y = 0
+		command_zero.linear.z = 0
+		pub.publish(command_zero)
+
 def takeoff():
         pub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10, latch=True)
         rate = rospy.Rate(10)
         t_end = time.time() + 7
         #while time.time() < t_end:
         pub.publish(Empty())
-        rate.sleep()
 
 def turn_in_place():
         pub = rospy.Publisher("cmd_vel", Twist, queue_size=10, latch=True)
         rate = rospy.Rate(10)
         command_rotate = Twist()
-        command_rotate.angular.z = 0.4 # spin the drone
+        command_rotate.angular.z = 0.3 # spin the drone
         t_end = time.time() + 3
         #while time.time() < t_end:
         pub.publish(command_rotate)
-        rate.sleep()
+        #rate.sleep()
 
 def move_forward():
         pub = rospy.Publisher("cmd_vel", Twist, queue_size=10, latch=True)
         rate = rospy.Rate(10)
         command_move = Twist()
-        command_move.linear.x = 0.2 # move drone forwards
+        command_move.linear.x = 0.1 # move drone forwards
 
         pub.publish(command_move)
-        rate.sleep()
+        #rate.sleep()
 
 def land():
         pub = rospy.Publisher("ardrone/land", Empty, queue_size=10, latch=True)
@@ -38,7 +50,7 @@ def land():
         t_end = time.time() + 7
         #while time.time() < t_end:
         pub.publish(Empty())
-        rate.sleep()
+        #rate.sleep()
 
 if __name__ == '__main__':
         rospy.init_node('drone_test1', anonymous=True)
@@ -46,15 +58,22 @@ if __name__ == '__main__':
         try:
           print("Taking off")
           takeoff()
-          time.sleep(2)
+          time.sleep(7)
         except rospy.ROSInterruptException:
           pass 
-        try:
-          print("Turning in place")
-          turn_in_place()
-          time.sleep(4)
-        except rospy.ROSInterruptException:
-          pass
+
+        # try:
+        #   print("zeroing out angular x and angular y")
+        #   zero_out_params()
+        #   #time.sleep(7)
+        # except rospy.ROSInterruptException:
+        #   pass
+        # try:
+        #   print("Turning in place")
+        #   turn_in_place()
+        #   time.sleep(4)
+        # except rospy.ROSInterruptException:
+        #   pass
         try:
           print("Moving forward")
           move_forward()
